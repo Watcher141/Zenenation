2c14b68698f03817421d6c54b6235491ee4b0c7e
Monojit <116868544+Watcher141@users.noreply.github.com>
2025-12-21 01:37:39 +0530

    @vercel/analytics/next

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,19 @@ import Home from "./pages/Home";
 import ProductDetail from "./pages/ProductDetail";
 import CategoryPage from "./pages/CategoryPage";
 import Footer from "./components/Footer";
+import { Analytics } from "@vercel/analytics/next"
 
 
 function App() {
   return (
     <BrowserRouter>
       <Navbar />
-
+      <Analytics />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/product/:id" element={<ProductDetail />} />
         <Route path="/category/:category" element={<CategoryPage />} />
-        
+
       </Routes>
       <Footer />
     </BrowserRouter>
